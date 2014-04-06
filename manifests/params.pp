495336fb4165b8e39c6d400ed6e82e5dc79a291c
Igor Galić <i.galic@brainsware.org>
2014-04-06 12:51:26 +0200

    Support more platforms
    currently, the only "weird" platform we have is Debian/Ubuntu, all others call this package 'ack'.
    With this simple "hack" we can support more platforms. YAY.

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,20 +1,6 @@
 class ack::params {
-  case $::osfamily {
-    debian: {
-      $package_name = 'ack-grep'
-    }
-    redhat: {
-      $package_name = 'ack'
-    }
-    default: {
-      case $::operatingsystem {
-        gentoo: {
-          $package_name = 'ack'
-        }
-        default: {
-          fail("Unsupported platform: ${::osfamily}/${::operatingsystem}")
-        }
-      }
-    }
+  $package_name = $::osfamily ? {
+    debian    => 'ack-grep',
+    default   => 'ack'
   }
 }
